8d7334681ea1104efe45a16567d564747d61bfb2
pydipalabhavana07@gmail.com <pydipalabhavana07@gmail.com>
2020-04-18 19:28:10 +0530

    commit

---
+++ ListEmployee-Config/src/main/java/com/cap/anurag/service/EmployeeService.java
@@ -0,0 +1,17 @@
+package com.cap.anurag.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.cap.anurag.entity.Employee;
+
+public interface EmployeeService {
+
+	
+    
+	List<Employee> findAllEmployees();
+    
+	Optional<Employee>  findEmployee(Integer empId);
+
+	
+	}
